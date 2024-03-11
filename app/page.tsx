ad780f7f67d52450b4813d22eab25259dadc6207
Klicksense <148848064+Klicksense@users.noreply.github.com>
2024-03-11 09:57:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function AuthenticationPage() {
           <div
             className="absolute inset-0 bg-cover bg-center brightness-75"
             style={{
-              backgroundImage: "url(/bgNew.jpeg)",
+              backgroundImage: "url(/image007.png)",
             }}
           />
           <div className="relative z-20 mt-10 flex w-full items-center justify-between text-lg font-medium">
